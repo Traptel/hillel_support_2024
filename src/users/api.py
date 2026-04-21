41be301134afac143b05254254c2b56042f23b2e
Nikita <traptel2004@gmail.com>
2026-04-21 19:15:18 +0200

    Fix

--- src/users/api.py
+++ src/users/api.py
@@ -151,22 +151,3 @@ def resend_activation_user(request) -> Response:
         {"message": "Activation mail resent to your email"},
         status=200,
     )
-
-
-# def create_user(request: HttpRequest) -> JsonResponse:
-#     if request.method != "POST":
-#         raise NotImplementedError("Only POST requsts")
-
-#     data: dict = json.loads(request.body)
-#     user = User.objects.create_user(**data)
-
-#     results = {
-#         "id": user.id,
-#         "email": user.email,
-#         "first_name": user.first_name,
-#         "last_name": user.last_name,
-#         "role": user.role,
-#         "is_active": user.is_active,
-#     }
-
-#     return JsonResponse(results)
