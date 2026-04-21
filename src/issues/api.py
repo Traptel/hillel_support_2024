b8c378e3b391ede8770f229205fd300243d1f07f
Nikita <traptel2004@gmail.com>
2026-04-21 19:07:29 +0200

    Fix CRLF to LF

--- src/issues/api.py
+++ src/issues/api.py
@@ -202,22 +202,3 @@ def issues_take(request: Request, id: int):
     serializer = IssueSerializer(issue)
 
     return response.Response(serializer.data)
-
-
-# @api_view(["GET"])
-# def fetch_issues(request):
-#     all_issues = Issue.objects.all()
-#     serialized_issues = IssueSerializer(all_issues, many=True)
-#     return Response(serialized_issues.data)
-
-
-# @api_view(["POST"])
-# def add_new_issue(request):
-#     received_data = request.data
-#     new_issue_serializer = IssueSerializer(data=received_data)
-
-#     if new_issue_serializer.is_valid():
-#         new_issue_serializer.save()
-#         return Response(new_issue_serializer.data)
-#     else:
-#         return Response(new_issue_serializer.errors)
